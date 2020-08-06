f116e2936a5e7e9e2fbcf596bf76f46e2d49eb3d
Vinay <vinay.choudhary1994@outlook.com>
2020-08-06 18:37:24 +0530

    Basic registration page

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+app.secret_key = 'your secret key'
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+
+	return render_template("index.html")
+
+if __name__ == "__main__":
+
+	app.run(debug=True)
