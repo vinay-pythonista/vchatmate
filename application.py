44908f5d4f73f1a02414f708f3b654e46cfb0c40
Vinay <vinay.choudhary1994@outlook.com>
2020-08-09 23:30:54 +0530

    Added message flashing

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 from passlib.hash import pbkdf2_sha256
 
@@ -43,6 +43,8 @@ def index():
 		db.session.add(user)
 		db.session.commit()
 		
+		flash('Registered successfully. Please login.', 'success')
+
 		return redirect(url_for('login'))
 
 	return render_template("index.html", form=reg_form)
@@ -67,8 +69,9 @@ def login():
 def chat():
 
 	if not current_user.is_authenticated:
-		return "Please login before accessing chat..."
-		
+		flash('Please login.', 'danger')
+		return redirect(url_for('login'))
+
 	return "Chat page"
 
 
@@ -76,7 +79,8 @@ def chat():
 def logout():
 
 	logout_user()
-	return "Logged out using flask-login :("
+	flash('You have logged out successfully.', 'success')
+	return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
