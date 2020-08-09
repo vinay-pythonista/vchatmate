6faecb02504653b726ce4202bc0b89af436dad64
Vinay <vinay.choudhary1994@outlook.com>
2020-08-09 22:31:57 +0530

    Login form and functionality added

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 from wtform_fields import *
 from models import *
@@ -27,9 +27,23 @@ def index():
 		user = User(username=username, password=password)
 		db.session.add(user)
 		db.session.commit()
-		return "User successfully registered!"
+		
+		return redirect(url_for('login'))
 
 	return render_template("index.html", form=reg_form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+
+	login_form = LoginForm()
+
+	# Allow login if validation is successful
+	if login_form.validate_on_submit():
+		return "Logged in, finally!"
+
+	return render_template('login.html', form=login_form)
+
 
 if __name__ == "__main__":
 
--- wtform_fields.py
+++ wtform_fields.py
@@ -4,6 +4,19 @@ from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
 from models import User
 
 
+def invalid_credentials(form, field):
+
+	username_entered = form.username.data
+	password_entered = field.data
+
+	# Check if username is valid
+	user_object = User.query.filter_by(username=username_entered).first()
+	if user_object is None:
+		raise ValidationError("Username or password is incorrect!")
+	elif password_entered != user_object.password:
+		raise ValidationError("Username or password is incorrect!")
+
+
 class RegistrationForm(FlaskForm):
 
 	username = StringField('username_label',
@@ -21,3 +34,13 @@ class RegistrationForm(FlaskForm):
 		user_object = User.query.filter_by(username=username.data).first()
 		if user_object:
 			raise ValidationError("Username already exists! Select a different username.")
+
+
+class LoginForm(FlaskForm):
+
+	username = StringField('username_label',
+		validators=[InputRequired(message="Username required.")])
+	password = PasswordField('password_label',
+		validators=[InputRequired(message="Password required."),
+		invalid_credentials])
+	submit_button = SubmitField('Login')
