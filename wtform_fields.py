a360039f8e787db73e27dce3cb09be52915b30a3
Vinay <vinay.choudhary1994@outlook.com>
2020-08-12 18:27:27 +0530

    Fixed the internal server error

--- wtform_fields.py
+++ wtform_fields.py
@@ -18,7 +18,7 @@ def invalid_credentials(form, field):
         raise ValidationError("Username or password is incorrect")
 
     # Check password in invalid
-    elif not pbkdf2_sha256.verify(password, user_data.password):
+    elif not pbkdf2_sha256.verify(password, user_data.hashed_pswd):
         raise ValidationError("Username or password is incorrect")
 
 
